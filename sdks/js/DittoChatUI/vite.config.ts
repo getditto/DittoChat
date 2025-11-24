2093e7ac1823d9aff22f27f90d4f2ae81006231e
Yuvaraj Nithyanandan <yuvaraj@quest1.io>
2025-11-24 19:04:47 +0530

    ditto chat ui test case fix

--- sdks/js/DittoChatUI/vite.config.ts
+++ sdks/js/DittoChatUI/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
-    css: true,
+    css: false,
     coverage: {
       provider: "v8",
       reporter: ["text", "json-summary", "json", "html"],
