d60f065a787bf206bc222871d3915aee11dfacec
bmalumphy <bryan.malumphy@ditto.com>
2025-12-11 14:27:37 -0500

    APP-951-New Architecture for Android

--- sdks/kotlin/src/main/java/com/ditto/dittochat/ui/DittoChatUI.kt
+++ sdks/kotlin/src/main/java/com/ditto/dittochat/ui/DittoChatUI.kt
@@ -2,6 +2,7 @@ package com.ditto.dittochat.ui
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.ditto.dittochat.ChatConfig
@@ -16,29 +17,27 @@ class DittoChatUI(
 
     @Composable
     fun RoomsListView(
-        navController: NavHostController = rememberNavController(),
-        roomsListScreenViewModel: RoomsListScreenViewModel
+        viewModel: RoomsListScreenViewModel,
+        navigateToChat: (String) -> Unit,
     ) {
         RoomsListScreen(
-            onNavigateToChat = { roomId ->
-                navController.navigate("chat/$roomId")
-            },
-            roomsListScreenViewModel
+            onNavigateToChat = navigateToChat,
+            viewModel = viewModel
         )
     }
 
     @Composable
     fun ChatRoomView(
+        viewModel: ChatScreenViewModel,
         roomId: String,
         retentionDays: Int? = null,
         onNavigateBack: () -> Unit,
-        chatScreenViewModel: ChatScreenViewModel
     ) {
         ChatScreen(
             roomId = roomId,
             retentionDays = retentionDays,
             onNavigateBack = onNavigateBack,
-            chatScreenViewModel
+            viewModel = viewModel
         )
     }
 
